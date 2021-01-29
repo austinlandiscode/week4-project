c28dc2fee4a4c589a8c3e612abab1417dde52729
Austin Landis <austinlandiscode@gmail.com>
2021-01-29 09:55:30 -0800

    create branching for prototype function.

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,25 @@ function Pizza(size, crust, sauce, toppings) {
   this.crust = crust;
   this.sauce = sauce;
   this.toppings = toppings;
+  this.pizzaPrice = 10;
+}
+
+Pizza.prototype.pizzaCharge = function() {
+  if (this.size === 'small 10"') {
+    this.pizzaPrice;
+  } else if (this.size === 'Medium 12"') {
+    this.pizzaPrice += 2;
+  } else if (this.size === 'Large 14"') {
+    this.pizzaPrice += 4;
+  } else {
+    this.pizzaPrice += 6;
+  }
+
+  if (this.crust === 'Pan' || this.crust === 'Stuffed') {
+    this.pizzaPrice += 2;
+  } else {
+    this.pizzaPrice;
+  }
+
+  
 }
