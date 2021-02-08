568175cd837216a6b032eaf10491b13fdf897a16
Austin Landis <austinlandiscode@gmail.com>
2021-02-07 16:08:40 -0800

    fix logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-function Pizza(size, crust, sauce, cheese, meattoppings, othertoppings) {
+// function ClientLog() {
+//   this.clients = {};
+//   this.currentId = 0;
+// }
+
+// ClientLog.prototype.addClient = function(pizza) {
+//   client.id = this.assignId();
+//   this.clients[client.id] = client;
+// }
+
+// ClientLog.prototype.assignId = function() {
+//   this.currentId += 1;
+//   return this.currentId;
+// }
+
+// ClientLog.prototype.findClient = function(id) {
+//   if (this.clients[id] != undefined) {
+//     return this.clients[id];
+//   }
+//   return false;
+// }
+
+function Pizza(name, size, crust, sauce, cheese, meattoppings, othertoppings) {
+  this.name = name;
   this.size = size;
   this.crust = crust;
   this.sauce = sauce;
@@ -9,20 +32,25 @@ function Pizza(size, crust, sauce, cheese, meattoppings, othertoppings) {
 }
 
 Pizza.prototype.pizzaCharge = function() {
-  if (this.size === 'Small 10"') {
+  if (this.size === '10" Small') {
     this.price;
-  } else if (this.size === 'Medium 12"') {
+  } else if (this.size === '12" Medium') {
     this.price += 2;
-  } else if (this.size === 'Large 14"') {
+  } else if (this.size === '14" Large') {
     this.price += 4;
   } else {
     this.price += 6;
   }
   return this.price
 }
+
+// let clientLog = new ClientLog();
+
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
+
+    let name = $("input#name").text();
     let size = $("select#size").val();
     let crust = $("select#crust").val();
     let sauce = $("select#sauce").val();
@@ -30,9 +58,9 @@ $(document).ready(function() {
     let meattoppings = $("select#meattoppings").val();
     let othertoppings = $("select#othertoppings").val();
 
-    let newPizza = new Pizza(size, crust, sauce, cheese, meattoppings, othertoppings)
-    console.log(newPizza);
+    let newPizza = new Pizza(name, size, crust, sauce, cheese, meattoppings, othertoppings);
+    // clientLog.addClient(newPizza);
     newPizza.pizzaCharge();
     $("#order").text(`You ordered a ${size} size pizza with ${crust} crust, ${sauce} sauce, ${cheese}, ${meattoppings}, and ${othertoppings}. That will be $${newPizza.price}!`)
-  })
+  });
 });
